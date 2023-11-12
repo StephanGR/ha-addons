b39571c118b78a619586a5c0367bf6a905488af6
GROULD Stéphan <grouldstephan@gmail.com>
2023-11-12 22:09:48 +0100

    update

--- wolgate/run.sh
+++ wolgate/run.sh
@@ -4,7 +4,6 @@ NEW_CONFIG="/config.json"
 
 WOL_MAC=$(jq --raw-output '.wol_macAddress' $CONFIG_PATH)
 WOL_BROADCAST=$(jq --raw-output '.wol_broadcastAddress' $CONFIG_PATH)
-PROXY_HOST=$(jq --raw-output '.proxyServer_host' $CONFIG_PATH)
 DOMAINS=$(jq --raw-output '.domains' $CONFIG_PATH)
 
 cat << EOF > $NEW_CONFIG
