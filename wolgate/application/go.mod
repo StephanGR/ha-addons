4d81b5bf6c11a9317a3490f9baf1212561eee2e9
GROULD Stéphan <grouldstephan@gmail.com>
2023-11-12 17:41:43 +0100

    fisrt commit

---
+++ wolgate/application/go.mod
@@ -0,0 +1,18 @@
+module github.com/StephanGR/proxy-plex
+
+go 1.21.3
+
+require (
+	github.com/mdlayher/wol v0.0.0-20220221231636-b763a792253a
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/josharian/native v1.0.0 // indirect
+	github.com/mdlayher/ethernet v0.0.0-20190313224307-5b5fc417d966 // indirect
+	github.com/mdlayher/packet v1.0.0 // indirect
+	github.com/mdlayher/socket v0.2.1 // indirect
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ wolgate/application/main.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mdlayher/wol"
+	"github.com/sirupsen/logrus"
+	"log"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+	"sync"
+	"time"
+)
+
+type Config struct {
+	Wol struct {
+		MacAddress       string `json:"macAddress"`
+		BroadcastAddress string `json:"broadcastAddress"`
+	} `json:"wol"`
+	ProxyServer struct {
+		Host string `json:"host"`
+		Port int    `json:"port"`
+	} `json:"proxyServer"`
+	Domains map[string]DomainConfig `json:"domains"`
+}
+
+type DomainConfig struct {
+	Address string `json:"address"`
+	Port    int    `json:"port"`
+}
+
+type ServerState struct {
+	wakingUp bool
+	lock     sync.Mutex
+}
+
+func initLogger() *logrus.Logger {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	return logger
+}
+
+func logRequest(logger *logrus.Logger, r *http.Request) {
+	logger.WithFields(logrus.Fields{
+		"client": r.RemoteAddr,
+		"method": r.Method,
+		"path":   r.URL.Path,
+	}).Info()
+}
+
+func (s *ServerState) IsWakingUp() bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	return s.wakingUp
+}
+
+func (s *ServerState) StartWakingUp() bool {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if s.wakingUp {
+		return false // Already waking up
+	}
+	s.wakingUp = true
+	return true
+}
+
+func (s *ServerState) DoneWakingUp() {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	s.wakingUp = false
+}
+
+func loadConfig(configPath string) (*Config, error) {
+	var config Config
+	configFile, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
+
+func isServerUp(address string) bool {
+	_, err := net.Dial("tcp", address)
+	return err == nil
+}
+
+func wakeServer(logger *logrus.Logger, macAddress string, broadcastAddress string, serverState *ServerState) {
+	if !serverState.StartWakingUp() {
+		logger.Info("There is already a wake up in progress")
+		return
+	}
+	defer serverState.DoneWakingUp()
+
+	client, err := wol.NewClient()
+	if err != nil {
+		logger.Warn("Error when creating WOL client : %v", err)
+		return
+	}
+	defer func(client *wol.Client) {
+		err := client.Close()
+		if err != nil {
+			logger.Warn("Unable to close the WOL client")
+		}
+	}(client)
+
+	mac, err := net.ParseMAC(macAddress)
+	if err != nil {
+		logger.Warn("Invalid mac address : %v", err)
+		return
+	}
+	if err := client.Wake(broadcastAddress, mac); err != nil {
+		logger.Warn("Error when sending magic packet : %v", err)
+	} else {
+		logger.Info("Magic packet sent")
+	}
+}
+
+func handleDomainProxy(w http.ResponseWriter, r *http.Request, domain DomainConfig) {
+	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
+		Scheme: "http",
+		Host:   fmt.Sprintf("%s:%d", domain.Address, domain.Port),
+	})
+
+	r.URL.Host = fmt.Sprintf("%s:%d", domain.Address, domain.Port)
+	r.URL.Scheme = "http"
+	r.Host = fmt.Sprintf("%s:%d", domain.Address, domain.Port)
+	proxy.ServeHTTP(w, r)
+}
+
+func handler(logger *logrus.Logger, w http.ResponseWriter, r *http.Request, config *Config, serverState *ServerState) {
+	domain, ok := config.Domains[r.Host]
+	if !ok {
+		http.Error(w, "Domain not configured", http.StatusNotFound)
+		return
+	}
+
+	serverAddress := fmt.Sprintf("%s:%d", domain.Address, domain.Port)
+
+	if !isServerUp(serverAddress) {
+		logger.Info("Plex server is offline, trying to wake up using Wake On Lan")
+		wakeServer(logger, config.Wol.MacAddress, config.Wol.BroadcastAddress, serverState)
+
+		timeout := time.After(1 * time.Minute)
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				if isServerUp(serverAddress) {
+					logger.Info("Server is up !")
+					externalUrl := fmt.Sprintf("http://%s", r.Host)
+					http.Redirect(w, r, externalUrl, http.StatusSeeOther)
+					return
+				} else {
+					logger.Info("Waiting for server to wake up...")
+				}
+			case <-timeout:
+				logger.Info("Timeout reached, server did not wake up.")
+				fmt.Fprintf(w, "Plex server is still offline. Please try again later.")
+				return
+			}
+		}
+	} else {
+		handleDomainProxy(w, r, domain)
+	}
+}
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+// Middleware pour logger les requêtes
+func requestLoggerMiddleware(logger *logrus.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logRequest(logger, r)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	logger := initLogger()
+	serverState := &ServerState{}
+	config, err := loadConfig("config.json")
+	if err != nil {
+		log.Fatal("Error loading config file: ", err)
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(logger, w, r, config, serverState)
+	})
+
+	mux.HandleFunc("/ping", PingHandler)
+
+	loggedMux := requestLoggerMiddleware(logger, mux)
+
+	logger.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", config.ProxyServer.Host, config.ProxyServer.Port), loggedMux))
+}
