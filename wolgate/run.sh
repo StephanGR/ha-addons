f7a058126b333b68b4f07dd5d3a7b6258a97ff07
GROULD Stéphan <grouldstephan@gmail.com>
2023-11-12 19:29:13 +0100

    update

--- wolgate/run.sh
+++ wolgate/run.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 CONFIG_PATH=/data/options.json
+NEW_CONFIG=/app/config.json
 
-# Exemple de lecture des options
-WOL_MAC_ADDRESS=$(jq --raw-output '.wol_mac_address' $CONFIG_PATH)
-WOL_BROADCAST_ADDRESS=$(jq --raw-output '.wol_broadcast_address' $CONFIG_PATH)
-PROXY_SERVER_HOST=$(jq --raw-output '.proxy_server_host' $CONFIG_PATH)
-PROXY_SERVER_PORT=$(jq --raw-output '.proxy_server_port' $CONFIG_PATH)
+WOL_MAC=$(jq --raw-output '.wol_macAddress' $CONFIG_PATH)
+WOL_BROADCAST=$(jq --raw-output '.wol_broadcastAddress' $CONFIG_PATH)
+PROXY_HOST=$(jq --raw-output '.proxyServer_host' $CONFIG_PATH)
+PROXY_PORT=$(jq --raw-output '.ports.appPort' $CONFIG_PATH)
+DOMAINS=$(jq --raw-output '.domains' $CONFIG_PATH)
 
-# Vous pouvez ensuite utiliser ces variables pour configurer votre application
-# Cela peut impliquer de générer un fichier de configuration ou de passer des arguments à votre application
+cat << EOF > $NEW_CONFIG
+{
+  "wol": {
+    "macAddress": "$WOL_MAC",
+    "broadcastAddress": "$WOL_BROADCAST"
+  },
+  "proxyServer": {
+    "host": "$PROXY_HOST",
+    "port": $PROXY_PORT
+  },
+  "domains": $DOMAINS
+}
+EOF
 
-# Lancer votre application
-exec /path/to/your/app --wol_mac_address "$WOL_MAC_ADDRESS" --wol_broadcast_address "$WOL_BROADCAST_ADDRESS" --proxy_server_host "$PROXY_SERVER_HOST" --proxy_server_port "$PROXY_SERVER_PORT"
+/app/wolgate
